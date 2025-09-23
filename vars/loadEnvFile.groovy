6e48ccc3f41bfdc187c23a812ad87db032bbdd29
Clark <clarkmillermail@gmail.com>
2025-09-23 14:07:08 -0400

    Refactored

--- vars/loadEnvFile.groovy
+++ vars/loadEnvFile.groovy
@@ -1,12 +1,7 @@
-def call(String projectName, String projectTier, String environment, String envFileName = "") {
-    String finalEnvFileName
-    if (envFileName == "") {
-        finalEnvFileName = ".env.${environment}"
-    } else {
-        finalEnvFileName = envFileName
-    }
+def call(String projectName, String projectTier, String environment, String envFileName = null) {
+    String finalEnvFileName = envFileName ?: ".env.${environment}"
     withCredentials([file(credentialsId: "${projectName}-${projectTier}-${environment}-env", variable: 'ENV_FILE')]) {
-        def secretContent = readFile(env.ENV_FILE)
+        String secretContent = readFile(env.ENV_FILE)
         writeFile file: finalEnvFileName, text: secretContent
     }
 }
