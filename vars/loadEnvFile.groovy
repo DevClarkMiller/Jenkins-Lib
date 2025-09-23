44a80fe1e3ff143d346c7b48c62acaf19033c470
Clark <clarkmillermail@gmail.com>
2025-09-23 14:00:25 -0400

    Added optional param to loadEnvFile to provide a custom over-ridden name

--- vars/loadEnvFile.groovy
+++ vars/loadEnvFile.groovy
@@ -1,6 +1,12 @@
-def call(String projectName, String projectTier, String environment) {
+def call(String projectName, String projectTier, String environment, String envFileName = "") {
+    String finalEnvFileName
+    if (envFileName == "") {
+        finalEnvFileName = ".env.${environment}"
+    } else {
+        finalEnvFileName = envFileName
+    }
     withCredentials([file(credentialsId: "${projectName}-${projectTier}-${environment}-env", variable: 'ENV_FILE')]) {
         def secretContent = readFile(env.ENV_FILE)
-        writeFile file: ".env.${environment}", text: secretContent
+        writeFile file: finalEnvFileName, text: secretContent
     }
 }
