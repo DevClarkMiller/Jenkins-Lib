33d5bdcf20aed388943014f97c14c81ddbe7e11e
Clark <clarkmillermail@gmail.com>
2025-10-15 15:29:20 -0400

    Hopefully made last fix

--- vars/scpBuildFilesToWWW.groovy
+++ vars/scpBuildFilesToWWW.groovy
@@ -1,5 +1,5 @@
 def call(String user, String serverDomain, String projectDomain, String buildDirName = "dist") {
     String path = "/var/www/${projectDomain}/html";
     sh "ssh ${user}@${serverDomain} 'mkdir -p ${path} && touch ${path}'"
-    sh "scp -r ${buildDirName}/* ${user}@${serverDomain}:path"
+    sh "scp -r ${buildDirName}/* ${user}@${serverDomain}:${path}"
 }
