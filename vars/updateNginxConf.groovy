71d2ccb3b472402e5db23353ffc22f57d7b06ed1
Clark <clarkmillermail@gmail.com>
2025-10-15 15:12:20 -0400

    Updated nginx conf command

--- vars/updateNginxConf.groovy
+++ vars/updateNginxConf.groovy
@@ -1,3 +1,4 @@
 def call(String user, String serverDomain, String siteName, String confName = "nginx.conf") {
     sh "scp ./${confName} ${user}@${serverDomain}:/etc/nginx/sites-available/${siteName}"
+    sh "ssh ${user}@${serverDomain} 'ln -s /etc/nginx/sites-available/${siteName}' /etc/nginx-sites-enabled"
 }
