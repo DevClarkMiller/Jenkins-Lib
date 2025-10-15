39dcfddb85fb0448c38d6a13f58ab788e1f8fc2c
Clark <clarkmillermail@gmail.com>
2025-10-15 15:28:31 -0400

    Hopefully made final fix

--- vars/updateNginxConf.groovy
+++ vars/updateNginxConf.groovy
@@ -1,6 +1,6 @@
 def call(String user, String serverDomain, String siteName, String confName = "nginx.conf") {
     String path = "/etc/nginx/sites-available${siteName}";
-    sh "ssh ./${confName} ${user}@${serverDomain} 'touch ${path}'"
+    sh "ssh 'touch ${path}'"
     sh "scp ./${confName} ${user}@${serverDomain}:${path}"
     sh "ssh ${user}@${serverDomain} 'ln -s ${path} /etc/nginx-sites-enabled"
 }
