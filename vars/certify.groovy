11120b465d248b6a8fe6d3af13d1478404a0cd93
Clark <clarkmillermail@gmail.com>
2025-10-16 18:33:00 -0400

    Modifed the certify function

--- vars/certify.groovy
+++ vars/certify.groovy
@@ -1,3 +1,3 @@
-def call(String user, String serverDomain, String siteName, String confName = "nginx.conf") {
+def call(String user, String serverDomain, String siteName) {
     sh "ssh ${user}@${serverDomain} 'sudo certbot --nginx -d ${siteName} -d www.${siteName} --non-interactive --agree-tos'"
 }
