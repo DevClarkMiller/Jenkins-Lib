5cfbee2a20f98ade159fd2db28a2721c7a977c69
Clark <clarkmillermail@gmail.com>
2025-11-23 17:44:16 -0500

    Updated nginx conf update script

--- vars/updateNginxConf.groovy
+++ vars/updateNginxConf.groovy
@@ -1,8 +1,11 @@
 def call(String user, String serverDomain, String siteName, String confName = "nginx.conf") {
-    String path = "/etc/nginx/sites-available/${siteName}";
-    sh "ssh ${user}@${serverDomain} 'touch ${path}'"
-    sh "scp ./${confName} ${user}@${serverDomain}:${path}"
-    sh "ssh ${user}@${serverDomain} 'ln -sf ${path} /etc/nginx/sites-enabled'"
-    sh "ssh ${user}@${serverDomain} 'sudo systemctl reload nginx.service'"
-    sh "ssh ${user}@${serverDomain} 'sudo certbot --nginx -d ${siteName} -d www.${siteName} --non-interactive --agree-tos'"
+    String path = "/etc/nginx/sites-available/${siteName}"
+
+    sh """
+        ssh ${user}@${serverDomain} 'touch ${path}'
+        scp ./${confName} ${user}@${serverDomain}:${path}
+        ssh ${user}@${serverDomain} 'ln -sf ${path} /etc/nginx/sites-enabled'
+        ssh ${user}@${serverDomain} 'sudo systemctl reload nginx.service'
+        ssh ${user}@${serverDomain} 'sudo certbot --nginx -d ${siteName} -d www.${siteName} --non-interactive --agree-tos'
+    """
 }
