dc26a6d18d8156a53fe831d19af5082e02e06b2c
Clark <clarkmillermail@gmail.com>
2025-11-23 19:21:13 -0500

    Added method to check if there were changes in microservice

---
+++ vars/checkMicroservice.groovy
@@ -0,0 +1,14 @@
+def call(String microserviceFolder) {
+	def changes = sh(
+        script: "git diff --name-only HEAD~1 | grep '^${microserviceFolder}/' || true",
+        returnStdout: true
+    ).trim()
+
+    if (!changes) {
+        echo "No changes in ${microserviceFolder}, skipping build"
+        currentBuild.result = 'SUCCESS'
+        return false // Indicates nothing changed
+    }
+
+    return true // Indicates there are changes
+}
