371be3214500c28328c3e01d77f8103039d200f5
Clark <clarkmillermail@gmail.com>
2025-11-23 20:56:03 -0500

    Removed unused param

--- vars/loadAppsettings.groovy
+++ vars/loadAppsettings.groovy
@@ -1,4 +1,4 @@
-def call(String projectName, String projectTier, String environment, String envFileName = null) {
+def call(String projectName, String environment, String envFileName = null) {
     String finalName = envFileName ?: "appsettings.${environment}.json"
     withCredentials([file(credentialsId: "${projectName}-${finalName}", variable: 'APP_SETTINGS_FILE')]) {
         String secretContent = readFile(env.APP_SETTINGS_FILE)
