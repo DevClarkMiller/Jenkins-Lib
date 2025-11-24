8f4bf93ce5d08b71e6b46d13d07a6bb9b5c59e6b
Clark <clarkmillermail@gmail.com>
2025-11-23 21:41:27 -0500

    Testing

--- vars/updateDBFromMigration.groovy
+++ vars/updateDBFromMigration.groovy
@@ -4,11 +4,18 @@ def call(String projectName, String context, String dotnetVersion, String efVers
 	-v "\$WORKSPACE:/workspace" \
 	-w /workspace \
 	mcr.microsoft.com/dotnet/sdk:${dotnetVersion} \
-	bash -c "dotnet tool install --global dotnet-ef --version ${efVersion} && \
-			export PATH=\\\$PATH:/root/.dotnet/tools && \
-			dotnet ef database update \
-			-s ./${projectName}.api \
-			-p ./${projectName}.data \
-			-c ${context} -- --environment Development"
+	bash -c "ls"
 	"""
+	// sh """
+	// docker run --rm \
+	// -v "\$WORKSPACE:/workspace" \
+	// -w /workspace \
+	// mcr.microsoft.com/dotnet/sdk:${dotnetVersion} \
+	// bash -c "dotnet tool install --global dotnet-ef --version ${efVersion} && \
+	// 		export PATH=\\\$PATH:/root/.dotnet/tools && \
+	// 		dotnet ef database update \
+	// 		-s ./${projectName}.api \
+	// 		-p ./${projectName}.data \
+	// 		-c ${context} -- --environment Development"
+	// """
 }
