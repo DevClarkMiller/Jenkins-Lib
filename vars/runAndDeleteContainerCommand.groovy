34075847cc59ef1e5cd79ec7b807650aa323df0c
Clark <clarkmillermail@gmail.com>
2025-11-23 20:36:49 -0500

    Fix command

--- vars/runAndDeleteContainerCommand.groovy
+++ vars/runAndDeleteContainerCommand.groovy
@@ -1,3 +1,3 @@
 def call(String imageName, String command) {
-    sh "docker run -it --rm -v '${PWD}:/workspace' -w /workspace ${imageName} && ${command}"
+    sh "docker run --rm -v '${PWD}:/workspace' -w /workspace ${imageName} && ${command}"
 }
