2876bb4ea6b307d21a2ba31efbe79cfddd59cae5
Clark <clarkmillermail@gmail.com>
2025-11-23 20:57:12 -0500

    Fix functino

--- vars/loadAppsettings.groovy
+++ vars/loadAppsettings.groovy
@@ -1,6 +1,6 @@
 def call(String projectName, String environment, String envFileName = null) {
     String finalName = envFileName ?: "appsettings.${environment}.json"
-    withCredentials([file(credentialsId: "${projectName}-${finalName}", variable: 'APP_SETTINGS_FILE')]) {
+    withCredentials([file(credentialsId: "${projectName}.${finalName}", variable: 'APP_SETTINGS_FILE')]) {
         String secretContent = readFile(env.APP_SETTINGS_FILE)
         writeFile file: finalName, text: secretContent
     }
