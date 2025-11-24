e4c2f356549451849a4eb3888c715bece0ae56f5
Clark <clarkmillermail@gmail.com>
2025-11-23 21:33:48 -0500

    Added new cheeky command to easily update DB

---
+++ vars/updateDBFromMigration.groovy
@@ -0,0 +1,14 @@
+def call(String projectName, String context, String dotnetVersion, String efVersion) {
+	sh """
+	docker run --rm \
+	-v "\$WORKSPACE:/workspace" \
+	-w /workspace \
+	mcr.microsoft.com/dotnet/sdk:${dotnetVersion} \
+	bash -c "dotnet tool install --global dotnet-ef --version ${efVersion} && \
+			export PATH=\\\$PATH:/root/.dotnet/tools && \
+			dotnet ef database update \
+			-s ./${projectName}.api \
+			-p ./${projectName}.data \
+			-c ${context} -- --environment Development"
+	"""
+}
