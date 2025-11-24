b7131c69cb9c630119c1d17ddf44ae935a259bf2
Clark <clarkmillermail@gmail.com>
2025-11-23 20:40:34 -0500

    Fix commad again

--- vars/runAndDeleteContainerCommand.groovy
+++ vars/runAndDeleteContainerCommand.groovy
@@ -1,4 +1,4 @@
 def call(String imageName, String command) {
 	echo command
-    sh "docker run --rm -v '${PWD}:/workspace' -w /workspace ${imageName} '${command}'"
+    sh "docker run --rm -v '${PWD}:/workspace' -w /workspace ${imageName} bash -c '${command}'"
 }
