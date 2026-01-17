429b66401395c2484f4e18b6b5a65ce0ba8f269f
DevClarkMiller <clarkmillermail@gmail.com>
2026-01-17 18:58:04 -0500

    Added in an optional redirect flag

--- vars/updateNginxConf.groovy
+++ vars/updateNginxConf.groovy
@@ -1,11 +1,12 @@
-def call(String user, String serverDomain, String siteName, String confName = "nginx.conf") {
+def call(String user, String serverDomain, String siteName, String confName = "nginx.conf", Boolean doRedirect = true) {
     String path = "/etc/nginx/sites-available/${siteName}"
+    String redirectFlag = doRedirect ? "--redirect" : ""
 
     sh """
         ssh ${user}@${serverDomain} 'touch ${path}'
         scp ./${confName} ${user}@${serverDomain}:${path}
         ssh ${user}@${serverDomain} 'ln -sf ${path} /etc/nginx/sites-enabled'
         ssh ${user}@${serverDomain} 'sudo systemctl reload nginx.service'
-        ssh ${user}@${serverDomain} 'sudo certbot --nginx -d ${siteName} -d www.${siteName} --non-interactive --agree-tos'
+        ssh ${user}@${serverDomain} 'sudo certbot --nginx -d ${siteName} -d www.${siteName} --non-interactive --agree-tos ${redirectFlag}'
     """
 }
